91c0cbbcb436bd5a070fcbd5a939135c88bb2466
Carolyn <ccpianist@gmail.com>
2018-11-05 14:46:05 -0800

    merge fx

--- mergesort.js
+++ mergesort.js
@@ -1,11 +1,19 @@
 function split(arr) {
     let firstHalf = arr.slice(0, (arr.length) / 2);
     let secondHalf = arr.slice((arr.length) / 2);
-    if (arr.length === 1) {
-        return [firstHalf, secondHalf];
-    }
-
+    return [firstHalf, secondHalf];
 }
 
+function merge(arr1, arr2) {
+    let result = [];
+    if (arr1[0] < arr2[0]){
+        result.push(arr1[0], arr2[0]);
+    } else {
+        result.push(arr2[0], arr1[0]);
+    } return result;
+}
+
+console.log(merge([54], [2]));
+
 let array = [1, 2, 3, 4, 5, 6];
-console.log(split(array))
+console.log(split(array));
